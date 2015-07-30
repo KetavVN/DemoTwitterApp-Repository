0623ee12ad13f076eb8ccd348116134ea295d5ef
Ketav Nathwani <ketav.nathwani@gmail.com>
2015-07-29 22:13:57 -0500

    Removed Comments

--- SpringREST/src/main/java/com/redis/test/aspects/RedisAspect.java
+++ SpringREST/src/main/java/com/redis/test/aspects/RedisAspect.java
@@ -37,57 +37,26 @@ public class RedisAspect {
 	
 	@Resource(name="redisTemplate")
 	private HashOperations<String, Object, Object> hashOps;
-
-	/*@Pointcut("execution(@com.redis.test.annotations.RCached * saveUser(..))")
-	public void savePC(){}*/
 	
 	@Pointcut(value="execution(@com.redis.test.annotations.RCached * saveUser(*)) "
 			+ "&& args(object) "
 			+ "&& @annotation(cached) "
 			+ "&& @args(cacheable)")
-	public void savePC2(Object object, RCached cached, RCacheable cacheable){}
+	public void savePointCut(Object object, RCached cached, RCacheable cacheable){}
 	
 	@Pointcut(value="execution(@com.redis.test.annotations.RCached * remove*(*)) "
 			+ "&& args(object) "
 			+ "&& @annotation(cached) "
 			+ "&& @args(cacheable)")
-	public void deletePC(Object object, RCached cached, RCacheable cacheable){}
+	public void deletePointCut(Object object, RCached cached, RCacheable cacheable){}
 	
 	@Pointcut(value="execution(@com.redis.test.annotations.RCached * find*(*)) && @annotation(cached)")
-	public void findPC(RCached cached){}
+	public void findPointCut(RCached cached){}
 	
-	
-	//first way - using raw reflection
-	/*@Around("savePC()")
-	public Object saveAdvice(ProceedingJoinPoint jp) throws Throwable {
-		try{
-			Object returnObj = jp.proceed();
-			RCached cached = jp.getTarget().getClass().getDeclaredMethod(jp.getSignature().getName(), User.class).getAnnotation(RCached.class);
-			String hashesName = cached.key();
-			Class<?> className = jp.getArgs()[0].getClass();
-			RCacheable cacheable = className.getAnnotation(RCacheable.class);
-			String fieldName = cacheable.keyField();
-			Method method = className.getDeclaredMethod("get"+StringUtils.capitalize(fieldName));
-			if(method==null)
-				return returnObj;
-			String hashKey = (String) method.invoke(jp.getArgs()[0]);
-			User user = (User) jp.getArgs()[0];
-			hashOps.put(hashesName, hashKey, user);
-			redisTemplate.expire(hashesName, cacheable.timeOut(), TimeUnit.SECONDS);
-			return returnObj;
-		} catch(Throwable e){
-			logger.error(e.getMessage(), e);
-			throw e;
-		} finally {
-			//ignore
-		}
-	}*/
-	
-	//second way - using @annotation and @args
-	@Around(value="savePC2(object, cached, cacheable)")
+	@Around(value="savePointCut(object, cached, cacheable)")
 	public Object saveAdvice2(ProceedingJoinPoint jp, Object object, RCached cached,
 			RCacheable cacheable) throws Throwable {
-		try{
+		try {
 			Object returnObj = jp.proceed();
 			String hashesName = cached.key();
 			String fieldName = cacheable.keyField();
@@ -98,17 +67,17 @@ public class RedisAspect {
 			hashOps.put(hashesName, hashKey, object);
 			redisTemplate.expire(hashesName, cacheable.timeOut(), TimeUnit.SECONDS);
 			return returnObj;
-		} catch(Throwable e){
+		} catch(Throwable e) {
 			logger.error(e.getMessage(), e);
 			throw e;
 		} finally {
-			//ignore
+			//
 		}
 	}
 	
-	@Around("findPC(cached)")
-	public Object findUserAdvice(ProceedingJoinPoint jp, RCached cached) throws Throwable {
-		try{
+	@Around("findPointCut(cached)")
+	public Object findAdvice(ProceedingJoinPoint jp, RCached cached) throws Throwable {
+		try {
 			Class<?> returnType = ((MethodSignature)jp.getSignature()).getReturnType();
 			RCacheable cacheable = returnType.getAnnotation(RCacheable.class);
 			Object keyValue = jp.getArgs()[0];
@@ -123,18 +92,18 @@ public class RedisAspect {
 			if(returnVal!=null)
 				hashOps.put(hashesName, keyValue, returnVal);
 			return returnVal;
-		} catch (Throwable e){
+		} catch (Throwable e) {
 			logger.error(e.getMessage(), e);
 			throw e;
 		} finally {
-			//ignore
+			//
 		}
 	}
 	
-	@Around("deletePC(object, cached, cacheable)")
-	public Object deleteUserAdvice(ProceedingJoinPoint jp, Object object,
+	@Around("deletePointCut(object, cached, cacheable)")
+	public Object deleteAdvice(ProceedingJoinPoint jp, Object object,
 			RCached cached, RCacheable cacheable) throws Throwable {
-		try{
+		try {
 			Object returnObj = jp.proceed();
 			String hashesName = cached.key();
 			String fieldName = cacheable.keyField();
@@ -144,11 +113,11 @@ public class RedisAspect {
 			Object hashKey = method.invoke(object);
 			hashOps.delete(hashesName, hashKey);
 			return returnObj;
-		} catch(Throwable e){
+		} catch(Throwable e) {
 			logger.error(e.getMessage(), e);
 			throw e;
 		} finally {
-			//ignore
+			//
 		}
 	}
 	
